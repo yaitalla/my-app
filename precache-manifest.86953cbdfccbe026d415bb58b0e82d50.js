5e37a3dbc7f245d256917ca6d41f9631149acb88
yaitalla <yaitalla.dev@gmail.com>
2020-04-19 11:29:37 +0200

    Updates

---
+++ precache-manifest.86953cbdfccbe026d415bb58b0e82d50.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2500938bb09b59b584dcd39ee1c4c80f",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "eb47f58b203f6f1d6d98",
+    "url": "/my-app/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "c894e8a8e01d6a599ce8",
+    "url": "/my-app/static/js/2.07a05dc4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-app/static/js/2.07a05dc4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb47f58b203f6f1d6d98",
+    "url": "/my-app/static/js/main.112c835c.chunk.js"
+  },
+  {
+    "revision": "7017a0a3783b2a9ce739",
+    "url": "/my-app/static/js/runtime-main.b51f6de3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
